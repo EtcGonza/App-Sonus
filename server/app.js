bed366baf0cffedffc320d4eb86836fb97d487e1
EGonza <xetchegaraygonzalo@gmail.com>
2019-04-18 15:15:37 -0300

    Se creo un JSON para poder probar como Back-End.

--- server/app.js
+++ server/app.js
@@ -1,5 +1,31 @@
 const fs = require('fs'); // Incluyo el modulo File System.
 
+// CUIDADO AL EJECUTAR, SE SOBREESCRIBIRA EL ARCHIVO MICROFONOS.JSON //
+
+function Microfono(modelo, clasificacion, nombre, descripcion, posiciones, urlSonidos, urlImagenes) {
+    this.modelo = modelo; // Bajo o Guitarra.
+    this.clasificacion = clasificacion; // Subclasificacion.
+    this.nombre = nombre; // Nombre del Microfono.
+    this.descripcion = descripcion; // Descripcion del microfono.
+    this.posiciones = posiciones; // Arreglo con posiciones Mid, Bridge o Neck.
+    this.urlSonidos = urlSonidos; // Arreglo Ulr's de los sonidos.
+    this.urlImagenes = urlImagenes // Arreglo con url's de imagenes.
+}
+
+var mic1 = new Microfono("Bajo",
+    "Bass Humbucker",
+    "Humbucker Bass Set", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac tempus ex, non ultricies dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque a nulla consectetur, pulvinar nisl id, volutpat diam. Etiam non tortor tortor. Etiam lacinia condimentum ipsum, nec tristique lectus fringilla eu. Suspendisse potenti. Nulla facilisi. Vivamus volutpat, ex ut mollis viverra, ante odio sagittis justo, vitae egestas neque nibh eget metus. Ut nisl nisi, molestie eget arcu a, dapibus vulputate erat. Suspendisse eget elit at enim efficitur efficitur. Nulla vel dolor augue. Proin lacinia nisl odio, at pretium ex pharetra vel. Praesent egestas eros nisi, sed gravida odio posuere vel. Pellentesque vehicula interdum nisl volutpat ultrices. Fusce finibus ultrices arcu, at malesuada libero semper a. Interdum et malesuada fames ac ante ipsum primis in faucibus.", ["Mid", "Bridge", "Neck"], ["https://soundcloud.com/sonus-fx/super-bridge-2018-drive-neck", "https://soundcloud.com/sonus-fx/super-bridge-2018-drive-neck-mid"], ["asdasdasd"]);
+
+let microfonos = [];
+
+microfonos.push(mic1);
+// microfonos.push(mic2);
+
+fs.writeFile('microfonos.json', JSON.stringify(microfonos, null, 2), 'utf8', (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+});
+
 // function MicrofonoGuitarra(nombre, tipo, colores, posiciones, urlSonidos, urlImagenes) {
 //     this.nombre = nombre; // Nombre del Microfono.
 //     this.tipo = tipo; // Simple Bobina o otros.
@@ -19,26 +45,3 @@ const fs = require('fs'); // Incluyo el modulo File System.
 //     this.urlSonidos = urlSonidos; // Ulr's de los sonidos.
 //     this.urlImagenes = urlImagenes // Arreglo con url's de imagenes.
 // }
-
-function Microfono(modelo, nombre, tipo, colores, posiciones, urlSonidos, urlImagenes) {
-    this.modelo = modelo; // Bajo o Guitarra.
-    this.nombre = nombre; // Nombre del Microfono.
-    this.tipo = tipo; // Simple Bobina o otros.
-    this.colores = colores; // Arreglo Colores en que viene.
-    this.posiciones = posiciones; // Arreglo con posiciones Mid, Bridge o Neck.
-    this.urlSonidos = urlSonidos; // Arreglo Ulr's de los sonidos.
-    this.urlImagenes = urlImagenes // Arreglo con url's de imagenes.
-}
-
-var mic1Guitarra = new Microfono("Guitarra", "SUPER BRIDGE Set x3", "Simple Bobina", ['Negro', 'Blanco'], ["Mid", "Bridge", "Neck"], ["https://soundcloud.com/sonus-fx/super-bridge-2018-drive-neck", "https://soundcloud.com/sonus-fx/super-bridge-2018-drive-neck-mid"], ["asdasdasd"]);
-var mic1Bajo = new Microfono("Bajo", "Jazz Bass Classic - Neck", "Simple Bobina", ['Negro'], ["Mid", "Bridge", "Neck"], ["https://soundcloud.com/sonus-fx/super-bridge-2018-drive-neck", "https://soundcloud.com/sonus-fx/super-bridge-2018-drive-neck-mid"], ["asdasdasdasd"]);
-
-let microfonos = [];
-
-microfonos.push(mic1Bajo);
-microfonos.push(mic1Guitarra);
-
-fs.writeFile('microfonos.json', JSON.stringify(microfonos, null, 2), 'utf8', (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-});
